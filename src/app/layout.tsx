e2adc018c3a23bbb4f94804c82a1fab99aef1e29
aryaw05 <aryawahyuaw77@gmail.com>
2025-12-04 18:55:36 +0700

    feat(layout): add Poppins font to root layout for better typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
+import { Poppins } from "next/font/google";
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={poppins.className}>
       <body className="antialiased">
         <ThemeProvider
           attribute="class"
